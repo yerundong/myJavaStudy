fdf57b7f7a23fba90b510f08d79aede17d94a596
yerundong <414868970@qq.com>
2020-08-11 23:14:15 +0800

    888

---
+++ JavaBase/src/类/抽象/AbstctExtendsAbstct.java
@@ -0,0 +1,13 @@
+package 类.抽象;
+
+public abstract class AbstctExtendsAbstct extends AbsctClass {
+
+    // 这里只重写了一个抽象方法，还有其他的抽象方法未重写
+    // 只能重写所有抽象方法，或也声明为一个抽象类
+
+    @Override
+    public void says() {
+        System.out.println("i am AbstctExtendsAbstct says");
+    }
+
+}
