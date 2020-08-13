ecfb79e5a5a2b139568a07cbe3cbb45144499ab2
yerundong <414868970@qq.com>
2020-08-14 00:08:22 +0800

    111

--- JavaBase/src/ArrayList/常用方法.java
+++ JavaBase/src/ArrayList/常用方法.java
@@ -8,7 +8,7 @@ public class 常用方法 {
     public static void main(String[] args) {
         // <M> add(E e)
         // <M> add(int index, E e)
-        // 返回boolean，表示是否添加成功（ArrayList一定会添加成功）
+        // <修改><返回> 修改原list，返回boolean，表示是否添加成功（ArrayList一定会添加成功）
         ArrayList<String> list = new ArrayList<>();
         list.add("李小龙");
         list.add("成龙");
@@ -18,12 +18,13 @@ public class 常用方法 {
         System.out.println(list);
 
         // <M> get(int index)
+        // <返回> 返回目标元素
         System.out.println(list.get(1));
 
         // <M> remove(int index)
-        // 返回被删除的元素
+        // <修改><返回> 修改原list，返回被删除的元素
         // <M> remove(Object o)
-        // 返回boolean是否成功
+        // <修改><返回> 修改原list，返回boolean是否成功
         String rm = list.remove(0);
         System.out.println(list);
         System.out.println(rm);
@@ -33,6 +34,7 @@ public class 常用方法 {
         System.out.println(rm2);
 
         // <M> size();
+        // <返回> 返回list长度
         int size = list.size();
         System.out.println(size);
 
---
+++ JavaBase/src/类/练习/发红包/User.java
@@ -0,0 +1,35 @@
+package 类.练习.发红包;
+
+public class User {
+    private String name;// 用户姓名
+    private int money;// 用户余额
+
+    public User() {
+    }
+
+    public User(String name, int money) {
+        this.name = name;
+        this.money = money;
+    }
+
+    // 展示用户信息（姓名、余额）
+    public void show(){
+        System.out.println("姓名：" + this.name + "，余额：" + this.money);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+}
