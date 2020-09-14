dfc091653ab42f122cecca8ef02323381fbe1152
yerundong <414868970@qq.com>
2020-09-15 00:21:09 +0800

    111

--- JavaBase/src/异常处理/TryCatch.java
+++ JavaBase/src/异常处理/TryCatch.java
@@ -1,4 +1,12 @@
 package 异常处理;
 
 public class TryCatch {
+    public static void main(String[] args) {
+        int[] arr1 = new int[1];
+        try {
+            System.out.println(arr1[1]);
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("出现数组索引越界异常");
+        }
+    }
 }
