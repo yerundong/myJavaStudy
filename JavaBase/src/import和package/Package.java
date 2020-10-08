d9aec197f29d0ba858ebdc6f2c30e5d1d802b816
yerundong <414868970@qq.com>
2020-10-08 21:53:59 +0800

    888

---
+++ JavaBase/src/import和package/Package.java
@@ -0,0 +1,18 @@
+package import和package;
+
+/*
+@ 包：包主要用来对类和接口进行分类。当开发 Java 程序时，可能编写成百上千的类，因此很有必要对类和接口进行分类。
+
+@ 包声明格式：package 包1.包2.包3
+
+@ 包命名规范：小驼峰（myPackage）
+
+@ 注意事项：
+    1.如果一个类定义在某个包中，那么 package 语句应该在源文件的首行。
+    2.package 语句对源文件中定义的所有类都有效。
+*/
+public class Package {
+    public static void main(String[] args) {
+
+    }
+}
