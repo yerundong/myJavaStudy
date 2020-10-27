cfa42d64fedbcb05adf8180b9b805aa497785f89
yerundong <414868970@qq.com>
2020-10-27 18:16:15 +0800

    333

---
+++ JavaBase/src/JUnit/Base.java
@@ -0,0 +1,40 @@
+package JUnit;
+
+import org.junit.Test;
+
+/*
+@ 简介：
+    JUnit是一个Java语言的单元测试框架。它由Kent Beck和Erich Gamma建立，逐渐成为源于Kent Beck的sUnit的xUnit家族中最为成功的一个。
+    JUnit有它自己的JUnit扩展生态圈。多数Java的开发环境都已经集成了JUnit作为单元测试的工具。
+    JUnit是由 Erich Gamma 和 Kent Beck 编写的一个回归测试框架（regression testing framework）。
+    Junit测试是程序员测试，即所谓白盒测试，因为程序员知道被测试的软件如何（How）完成功能和完成什么样（What）的功能。
+    Junit是一套框架，继承TestCase类，就可以用Junit进行自动测试了。
+
+@ 要求：
+    1.测试类是公共的
+    2.测试类只能有一个公共无参的构造函数
+*/
+public class Base {
+    private String name;
+    private int age;
+
+    public void look(){
+        System.out.println(name);
+    }
+
+    @Test
+    public void test_look() {
+        name = "张三";
+        look();
+    }
+
+    public void says(String str){
+        System.out.println(str);
+    }
+
+    @Test
+    public void testSays(){
+        says("hello junit");
+    }
+
+}
--- JavaBase/src/类和对象/包装类.java
+++ JavaBase/src/类和对象/包装类.java
@@ -1,10 +1,14 @@
 package 类和对象;
 
 /*
-@包装类(Wrapper Class)：
-    java语言是一个面向对象的语言，但并不是纯面向对象的语言，java中的基本数据类型不是面向对象的，我们在实际使用中经常将基本数据类型转换成对象，
-    便于操作，比如，集合的操作中，这时，我们就需要利用包装类将基本类型数据转化成对象！
+@ 包装类(Wrapper Class)：
+    java语言是一个面向对象的语言，但并不是纯面向对象的语言。基本数据类型不是面向对象的，它们不具有继承性、多态性、封装性。
+    在实际使用中经常将基本数据类型转换成对象，便于操作，比如，集合的操作中，这时，我们就需要利用包装类将基本类型数据转化成对象！
 
+@ 特性：
+    1.基本类型不可调用方法、属性等对象才有的特性
+    2.包装类不可直接使用算术运算符，需要先转成基本类型
+    3.从JDK 1.5+开始，支持自动装箱、自动拆箱，可省略装箱、拆箱的步骤
 
 @ 基本类型与包装类的对应关系：
     基本类型      包装类（引用类型，包装类都位于java.lang包下）
@@ -30,75 +34,79 @@ package 类和对象;
 */
 public class 包装类 {
     public static void main(String[] args) {
-        // 装箱
-        // byte bt = new Byte((byte)126);
-        byte bt = new Byte("126");
+        // @ 装箱
 
-        // short st = new Short((short)123);
-        short st = new Short("123");
+        // @ Byte
+        // Byte(byte value)
+        // Byte(String s)：s必须是数字字符串
+        Byte bt = new Byte("123");
 
+        // @ Short
+        // Short(short value)
+        // Short(String s)：s必须是数字字符串
+        Short st = new Short("123");
+
+        // @ Integer
+        // Integer(int value)
+        // Integer(String s)：s必须是数字字符串
         Integer num = new Integer(55);
-        // Integer num = new Integer("55");
 
-        long lng = new Long(66);
-        // long lng = new Long("66");
+        // @ Long
+        // Long(long value)
+        // Long(String s)：s必须是数字字符串
+        Long lng = new Long(66);
 
-        float ft = new Float(55.66);
-        // float ft = new Float("55.66");
+        // @ Float
+        // Float(double value)
+        // Float(float value)
+        // Float(String s)：s必须是数字字符串
+        Float ft = new Float(55.66);
 
-        double db = new Double(99.77);
-        // double db = new Double("99.77");
+        // @ Double
+        // Double(double value)
+        // Double(String s)：s必须是数字字符串
+        Double db = new Double(99.77);
 
+        // @ Character
+        // Character(char value)
         Character ct = new Character('a');
 
-        boolean bol = new Boolean(false);
-        // boolean bol = new Boolean("false");
+        // @ Boolean
+        // Boolean(boolean value)
+        // Boolean(String s)：s推荐是布尔值字符串（忽略大小写），布尔值字符串返回对应布尔值，其他都返回false
+        Boolean bol = new Boolean(false);
+        Boolean bol1 = new Boolean("True");// true
+        Boolean bol2 = new Boolean("true");// true
+        Boolean bol3 = new Boolean("false");// false
+        Boolean bol4 = new Boolean("ttt");// false
+        Boolean bol5 = new Boolean("123");// false
 
-        // 拆箱
-        byte bt_ = Byte.valueOf(bt);
-        short st_ = Short.valueOf(st);
-        int num_ = Integer.valueOf(num);
-        long lng_ = Long.valueOf(lng);
-        float ft_ = Float.valueOf(ft);
-        double db_ = Double.valueOf(db);
-        char ct_ = Character.valueOf(ct);
-        boolean bol_ = Boolean.valueOf(bol);
+        // @ 装箱之后具有类和对象的特性，如下：
+        bt.equals(bol3);
+        ct.toString();
 
-        System.out.println(bt);
-        System.out.println(bt_);
+        // @ 拆箱
+        // xxxValue()
+        byte bt_ = bt.byteValue();
+        short st_ = st.shortValue();
+        int num_ = num.intValue();
+        long lng_ = lng.longValue();
+        float ft_ = ft.floatValue();
+        double db_ = db.doubleValue();
+        char ct_ = ct.charValue();
+        boolean bol_ = bol.booleanValue();
 
-        System.out.println(st);
-        System.out.println(st_);
-
-        System.out.println(num);
-        System.out.println(num_);
-
-        System.out.println(lng);
-        System.out.println(lng_);
-
-        System.out.println(ft);
-        System.out.println(ft_);
-
-        System.out.println(db);
-        System.out.println(db_);
-
-        System.out.println(ct);
-        System.out.println(ct_);
-
-        System.out.println(bol);
-        System.out.println(bol_);
-
-        // 自动装箱
+        // @ 自动装箱
+        // 从JDK 1.5+开始，支持自动装箱、自动拆箱
         Byte bt1 = 123;
         Integer int1 = 1;
         Boolean bool1 = true;
         boolean bool2 = true;
         bool1.equals(true);
-        // bool2.equals(true);// 报错
 
-        // ...
-
-        // 自动拆箱
-        int1 = int1+1;
+        // @ 自动拆箱
+        byte bt1_ = bt1;
+        int int1_ = int1;
+        int1.equals(true);// 报错
     }
 }
