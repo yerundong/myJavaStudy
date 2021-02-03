9729f3855cb345a12a664e0a9966f1e63683f1a0
yerundong <414868970@qq.com>
2021-02-03 17:42:27 +0800

    333

--- JavaBase/src/集合框架/ArrayList集合/常用方法.java
+++ JavaBase/src/集合框架/ArrayList集合/常用方法.java
@@ -1,29 +1,36 @@
 package 集合框架.ArrayList集合;
 
+import org.junit.jupiter.api.Test;
+
 import java.util.ArrayList;
 import java.util.Random;
 
 public class 常用方法 {
-    public static void main(String[] args) {
+    @Test
+    public void test_add() {
         // <M> add(E e)
         // <M> add(int index, E e)
-        // <修改><返回> 修改原list，返回boolean，表示是否添加成功（ArrayList一定会添加成功）
+        // <改、返> 修改原list，返回boolean，表示是否添加成功（ArrayList一定会添加成功）
         ArrayList<String> list = new ArrayList<>();
         list.add("李小龙");
         list.add("成龙");
+        System.out.println(list);
         // 把原位置的元素往后面挤
         list.add(1, "李连杰");
         list.add(1, "甄子丹");
         System.out.println(list);
+    }
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
 
         // <M> get(int index)
-        // <返回> 返回目标元素
+        // <返> 返回目标元素
         System.out.println(list.get(1));
 
         // <M> remove(int index)
-        // <修改><返回> 修改原list，返回被删除的元素
+        // <改、返> 修改原list，返回被删除的元素
         // <M> remove(Object o)
-        // <修改><返回> 修改原list，返回boolean是否成功
+        // <改、返> 修改原list，返回boolean是否成功
         String rm = list.remove(0);
         System.out.println(list);
         System.out.println(rm);
@@ -33,7 +40,7 @@ public class 常用方法 {
         System.out.println(rm2);
 
         // <M> size();
-        // <返回> 返回list长度
+        // <返> 返回list长度
         int size = list.size();
         System.out.println(size);
 
